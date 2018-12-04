1501388ef736e7f82394d8ff8914581eec1e5525
Michael Kovalski <michaelhkovalski@gmail.com>
2018-12-04 11:11:37 -0500

    updating mcts.py

--- mcts.py
+++ mcts.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import numpy as np
-from rubiks import Cube3x3 as Cube
+from rubiks import Cube2x2, Cube3x3
 from network import ADINetwork
 import tensorflow as tf
 import copy
 import argparse
 import pdb
 import sys
+import time
 
 class Node(object):
     ''' A node in the MCTS algorithm. Contains information such as number
@@ -69,25 +70,22 @@ class Node(object):
 
         return value, self.cube.is_solved()
 
-    def search(self, network, move_num = -1):
+    def search(self, network, move_num = 0):
         if self.is_leaf():
             value, solved = self.expand(network)
-            return value, solved
+            return value, solved, move_num
         else:
             action = self.policy()
-            if move_num == 1:
-                print(action)
-                #pdb.set_trace()
-            value, solved = self.children[action].search(network)
+            value, solved, mn = self.children[action].search(network, move_num = move_num + 1)
             self.w_s[action] = np.max([value, self.w_s[action]])
             self.n_s[action] += 1
             self.l_s[action] -= self.v
-            return value, solved
+            return value, solved, mn
 
 class MCTS(object):
-    def __init__(self, checkpoint, exploration = 0.1, virt_loss = 0.1):
+    def __init__(self, checkpoint, cube, exploration = 0.1, virt_loss = 0.1):
         self.network = ADINetwork(output_dir = checkpoint, use_gpu = False)
-        self.network.setup(Cube().cube.size)
+        self.network.setup(cube.cube.size)
         self.max_steps = 200
         
         # Hyperparameters
@@ -95,43 +93,75 @@ class MCTS(object):
         self.c = exploration
         self.v = virt_loss
 
-    def search(self, cube):
+    def search(self, cube, time_limit = 60):
+        
         root_node = Node(None, cube, self.c, self.v, self.network)
         
         num_moves = 0
-        while True:
-            _, solved= root_node.search(self.network, move_num = 1)
+        t1 = time.time()
+       
+        solved = False
+        while(time.time() - t1 < time_limit):
+            _, solved, move_steps= root_node.search(self.network, move_num = 0)
             if solved:
-                print("SOLVED PUZZLE YEEEEAAAAHHHHH")
-                print("Solved in {} moves".format(num_moves))
                 break
             num_moves += 1
+
+        return solved, num_moves, move_steps
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Solve a single puzzle using MCTS")
     parser.add_argument("-c", "--checkpoint", required=True, help="tf checkpoint for network")
     parser.add_argument("-e", "--exploration", default = 0.1, type = float, help = "exploration hyperparameter")
     parser.add_argument("-v", "--v_loss", default = 0.000001, type = float, help = "virtual loss hyperparameter")
+    parser.add_argument("-d", "--dims", default = 2, choices = [2, 3], type = int, help = "Cube dimensions")
+    parser.add_argument("-b", "--batch", default = 1, type = int)
+    parser.add_argument("--verbose", action = "store_true")
+    parser.add_argument("-m", "--moves", type = int, default = 5)
+    parser.add_argument("-t", "--time", type = int, help = "Time limit", default = 60)
 
     args = parser.parse_args()
-
-    mcts = MCTS(args.checkpoint, args.exploration, args.v_loss)
     
     # Shuffle a cube a bunch
-    cube = Cube()
+    if args.dims == 2:
+        cube = Cube2x2()
+    else:
+        cube = Cube3x3()
+    
+    mcts = MCTS(args.checkpoint, cube, args.exploration, args.v_loss)
+    
     actions = np.eye(12)
     
-    win_moves = []
-    for i in range(6):
-        action = np.random.choice(np.arange(0, actions.shape[0]))
-        cube.move(actions[action, :])
-        flip = 1 if action % 2 == 0 else -1
-        win_moves.append(action + flip)
+    num_solved = 0
+    total_cubes = args.batch
 
-    for it in reversed(win_moves):
-        print(" - Take action {}".format(it))
+    nodes_explored = []
+    solve_length = []
 
-    print("Starting search")
-    mcts.search(cube)
+    for b in range(args.batch):
+        cube.reset()
+        win_moves = []
+        for i in range(args.moves):
+            action = np.random.choice(np.arange(0, actions.shape[0]))
+            cube.move(actions[action, :])
+            flip = 1 if action % 2 == 0 else -1
+            win_moves.append(action + flip)
+        
+        if args.verbose:
+            for it in reversed(win_moves):
+                print(" - Take action {}".format(it))
 
+            print("Starting search")
+
+        solved, num_moves, move_steps = mcts.search(cube, time_limit = args.time)
+        if solved:
+            num_solved += 1
+            nodes_explored.append(num_moves)
+            solve_length.append(move_steps)
+    
+
+    print("Statistics: solving {} moves away".format(args.moves))
+    print(" - Percent solved: {}".format(num_solved / float(total_cubes)))
+    print(" - Average num nodes explored: {}".format(np.mean(nodes_explored)))
+    print(" - Average number of steps to solve length: {}".format(np.mean(solve_length)))
 
